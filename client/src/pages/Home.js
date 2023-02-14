1a6e51f0851c89b5bd88a1f7bf6278513fe12891
Anthony <almogotlane@gmail.com>
2023-02-14 15:10:04 +0200

    Imported MenuBar component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import MenuBar from "../components/MenuBar";
 
 import "./Home.css";
 import logo from "./logo.svg";
@@ -24,20 +25,25 @@ export function Home() {
 	}, []);
 
 	return (
-		<main role="main">
-			<div>
-				<img
-					className="logo"
-					data-qa="logo"
-					src={logo}
-					alt="Just the React logo"
-				/>
-				<h1 className="message" data-qa="message">
-					{message}
-				</h1>
-				<Link to="/about/this/site">About</Link>
-			</div>
-		</main>
+		<div>
+			<header>
+				<MenuBar />
+			</header>
+			<main role="main">
+				<div>
+					<img
+						className="logo"
+						data-qa="logo"
+						src={logo}
+						alt="Just the React logo"
+					/>
+					<h1 className="message" data-qa="message">
+						{message}
+					</h1>
+					<Link to="/about/this/site">About</Link>
+				</div>
+			</main>
+		</div>
 	);
 }
 
