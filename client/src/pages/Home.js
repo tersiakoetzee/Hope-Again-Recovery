f113e8a47d97ae3881c3e817091c78683571587f
Nishka Kisten <nishka.crypto@gmail.com>
2023-02-16 18:59:48 +0200

    Made changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import PicCarousel from "../components/carousel";
+import { PicCarousel } from "../components/PicCarousel";
 export function Home() {
 	const [message, setMessage] = useState("Loading...");
 	useEffect(() => {
