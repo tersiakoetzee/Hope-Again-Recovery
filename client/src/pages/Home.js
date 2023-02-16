d9dc56e55141a33f79f3ec557029b2ff7309388e
Nishka Kisten <nishka.crypto@gmail.com>
2023-02-16 17:38:50 +0200

    Picture carousel added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,9 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
 import "./Home.css";
-import logo from "./logo.svg";
-
+import PicCarousel from "../components/carousel";
 export function Home() {
 	const [message, setMessage] = useState("Loading...");
-
 	useEffect(() => {
 		fetch("/api")
 			.then((res) => {
@@ -26,16 +23,11 @@ export function Home() {
 	return (
 		<main role="main">
 			<div>
-				<img
-					className="logo"
-					data-qa="logo"
-					src={logo}
-					alt="Just the React logo"
-				/>
 				<h1 className="message" data-qa="message">
 					{message}
 				</h1>
 				<Link to="/about/this/site">About</Link>
+				<PicCarousel />
 			</div>
 		</main>
 	);
