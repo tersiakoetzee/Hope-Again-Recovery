48895a33e4d3348855f39df85e6517e6be34daa1
chezlinbenson <chezlinbenson11@gmail.com>
2023-02-23 06:37:03 +0200

    Added Testimonial content and fonts

--- client/src/components/TestimonialWidget.js
+++ client/src/components/TestimonialWidget.js
@@ -12,13 +12,15 @@ export const TestimonialWidget = () => {
 				alt="Card image"
 			/>
 			<Card.ImgOverlay>
-				<Card.Title className="card-title">Card title</Card.Title>
-				<Card.Text className="card-para">
-					This is a wider card with supporting text below as a natural lead-in
-					to additional content. This content is a little bit longer.
+				<Card.Title className="card-title" id="testimonial-title">
+					Testimonials
+				</Card.Title>
+				<Card.Text className="card-para" id="testimonial-para">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+					varius enim in eros elementum tristique. Duis cursus, mi quis viverra
+					ornare, eros dolor interdum nulla, ut .
 				</Card.Text>
-				<Card.Text>Last updated 3 mins ago</Card.Text>
-			</Card.ImgOverlay>
+				</Card.ImgOverlay>
 		</Card>
 	);
 };
