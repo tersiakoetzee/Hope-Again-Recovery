615b4036c4d59ee23e66aede66167c18fff396ca
Tersia Koetzee <tersiakoetzee@gmail.com>
2023-03-01 10:53:47 +0200

    added breakpoints to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,19 @@ import { About } from "./pages/About";
 import Home from "./pages/Home";
 import { Projects } from "./pages/Projects";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
 
 const App = () => (
+		<ThemeProvider
+				breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+				minBreakpoint="xxs"
+			>
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about" element={<About />} />
 		<Route path="/projects" element={<Projects />} />
 	</Routes>
+	</ThemeProvider>
 );
 
 export default App;
