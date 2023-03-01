e14d093ae4ee5551f305dc377948738fc5eb974c
Tersia Koetzee <tersiakoetzee@gmail.com>
2023-03-01 11:36:22 +0200

    fixing ci problems

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ThemeProvider from "react-bootstrap/ThemeProvider";
 
 const App = () => (
-		<ThemeProvider
-				breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
-				minBreakpoint="xxs"
-			>
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about" element={<About />} />
-		<Route path="/projects" element={<Projects />} />
-	</Routes>
+	<ThemeProvider
+		breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+		minBreakpoint="xxs"
+	>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/projects" element={<Projects />} />
+		</Routes>
 	</ThemeProvider>
 );
 
