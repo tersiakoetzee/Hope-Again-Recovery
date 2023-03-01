c8aa9f0c84d70027c18bf9d859347d9562898712
Tersia Koetzee <tersiakoetzee@gmail.com>
2023-03-01 13:23:55 +0200

    program content added

--- client/src/pages/Program.js
+++ client/src/pages/Program.js
@@ -3,50 +3,49 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-
-export const Program = () =>{
-    return (
-			<Template>
-				<Container>
-					<Row>
-						<Col className="text-center py-3">
-							<h3>After Care Program</h3>
-							<p>
-								6-9 month inhouse program focused on: One on one counselling
-								Comprehensive assessments Group counselling Support groups
-								Family restoration Support in a safe, sober living environment
-								Referrals to facilitate re-integration Contact made with
-								precious employers (if indicated)
-							</p>
-						</Col>
-						<Col className="text-center py-3">
-							<img
-								src="https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/280253799_745221696846746_6212596363167311079_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=0EsRaksBibsAX-MyOrZ&_nc_ht=scontent-cpt1-1.xx&oh=00_AfADm-pQDtj14jsIewKly8j0diivgPlDLmDS8wE3DOj-4g&oe=64034DA5"
-								alt="First slide"
-								height="150"
-							/>
-						</Col>
-					</Row>
-					<Row>
-						<Col className="text-center py-3">
-							<img
-								src="https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/280253799_745221696846746_6212596363167311079_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=0EsRaksBibsAX-MyOrZ&_nc_ht=scontent-cpt1-1.xx&oh=00_AfADm-pQDtj14jsIewKly8j0diivgPlDLmDS8wE3DOj-4g&oe=64034DA5"
-								alt="First slide"
-								height="150"
-							/>
-						</Col>
-						<Col className="text-center py-3">
-							<h3>After Care Program</h3>
-							<p>
-								6-9 month inhouse program focused on: One on one counselling
-								Comprehensive assessments Group counselling Support groups
-								Family restoration Support in a safe, sober living environment
-								Referrals to facilitate re-integration Contact made with
-								precious employers (if indicated)
-							</p>
-						</Col>
-					</Row>
-				</Container>
-			</Template>
-		);
+export const Program = () => {
+	return (
+		<Template>
+			<Container>
+				<Row className="square rounded border border-3 bg-light ">
+					<Col className="text-center py-3">
+						<h3>After Care Program</h3>
+						<p>
+							6-9 month inhouse program focused on:<br></br>
+							One on one counselling Comprehensive assessments Group counselling
+							Support groups Family restoration Gardening Project Support in a
+							safe, sober living environment Contact made with precious
+							employers (if indicated)
+						</p>
+					</Col>
+					<Col className="text-center py-3 square rounded border border-3 bg-secondary">
+						<img
+							src="https://images.unsplash.com/photo-1490578474895-699cd4e2cf59?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTJ8fGdyb3VwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+							alt="First slide"
+							height="250"
+						></img>
+					</Col>
+				</Row>
+				<Row className="square rounded border border-3  bg-light">
+					<Col className="text-center py-3">
+						<h3>Early Intervention Program</h3>
+						<p>
+							Schools and community program targeted at ages 13-21 years:
+							<br></br>
+							Motivational Interviews Comprehensive assessments One-on-one
+							counselling with a social worker Group counselling Support Groups
+							Family restoration Random Multi-drug urine testing
+						</p>
+					</Col>
+					<Col className="text-center py-3 square rounded border border-3 bg-secondary">
+						<img
+							src="https://images.unsplash.com/photo-1516149893016-813d9a01d5d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8eW91dGh8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+							alt="First slide"
+							height="250"
+						></img>
+					</Col>
+				</Row>
+			</Container>
+		</Template>
+	);
 };
