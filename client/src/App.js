0a25b3ae0cb49e5220070a44efd8fef03f9cc625
Tersia <48313801+tersiakoetzee@users.noreply.github.com>
2023-03-02 11:26:03 +0200

    project content added (#56)
    * project content added
    
    * ci issues
    
    * ci issues
    
    * added break points for responsiveness
    
    * added breakpoints to App.js
    
    * fixing ci problems
    
    * ci issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import { About } from "./pages/About";
 import Home from "./pages/Home";
+import { Projects } from "./pages/Projects";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
 
 const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about" element={<About />} />
-	</Routes>
+	<ThemeProvider
+		breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+		minBreakpoint="xxs"
+	>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/projects" element={<Projects />} />
+		</Routes>
+	</ThemeProvider>
 );
 
 export default App;
