4d77fc4baaf81ee438f001c37d22d8a6998d6ca8
Tersia <48313801+tersiakoetzee@users.noreply.github.com>
2023-03-02 18:29:30 +0200

    Program (#59)
    * setting up program page
    
    * program content added
    
    * Family day content added
    
    * ci fixed
    
    * Images fixed
    
    * Ci issues fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { About } from "./pages/About";
 import Home from "./pages/Home";
+import { Program } from "./pages/Program";
+import { FamilyDays } from "./pages/FamilyDays";
 import { Projects } from "./pages/Projects";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ThemeProvider from "react-bootstrap/ThemeProvider";
@@ -14,6 +16,8 @@ const App = () => (
 			<Route path="/" element={<Home />} />
 			<Route path="/about" element={<About />} />
 			<Route path="/projects" element={<Projects />} />
+			<Route path="/program" element={<Program />} />
+			<Route path="/familydays" element={<FamilyDays />} />
 		</Routes>
 	</ThemeProvider>
 );
