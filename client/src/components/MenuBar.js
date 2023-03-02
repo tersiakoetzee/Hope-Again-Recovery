0d19dbbc5d79630a2b6c97f59be6d1358198e185
Anthony Mogotlane <almogotlane@gmail.com>
2023-03-02 19:51:00 +0200

    Updated the menu bar

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -2,18 +2,27 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import "./MenuBar.css";
 
 export const MenuBar = () => {
 	return (
 		<Navbar bg="dark" variant="dark" expand="lg">
-			<Container>
+			<Container className="justify-content-center flex-grow-1">
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
+				<Navbar.Collapse className="text-center" id="basic-navbar-nav">
 					<Nav className="me-auto justify-content-center flex-grow-1">
-						<Nav.Link href="/">Home</Nav.Link>
-						<Nav.Link href="/about">About</Nav.Link>
+						<Nav.Link className="b-start b-right px-4" href="/">
+							Home
+						</Nav.Link>
+						<Nav.Link className="b-right b-left px-4" href="/about">
+							About
+						</Nav.Link>
 						{/* Program dropdown */}
-						<NavDropdown title="Program" id="basic-nav-dropdown">
+						<NavDropdown
+							className="b-right b-left px-4"
+							title="Program"
+							id="basic-nav-dropdown"
+						>
 							<NavDropdown.Item href="#action/3.1">
 								Program Outlay
 							</NavDropdown.Item>
@@ -22,14 +31,22 @@ export const MenuBar = () => {
 							</NavDropdown.Item>
 						</NavDropdown>
 						{/* Projects dropdown */}
-						<NavDropdown title="Projects" id="basic-nav-dropdown">
+						<NavDropdown
+							className="b-right b-left px-4"
+							title="Projects"
+							id="basic-nav-dropdown"
+						>
 							<NavDropdown.Item href="#action/3.1">Land</NavDropdown.Item>
 							<NavDropdown.Item href="#action/3.3">
 								Future Plans
 							</NavDropdown.Item>
 						</NavDropdown>
 						{/* Screening dropdown */}
-						<NavDropdown title="Screening" id="basic-nav-dropdown">
+						<NavDropdown
+							className="b-right b-left px-4"
+							title="Screening"
+							id="basic-nav-dropdown"
+						>
 							<NavDropdown.Item href="#action/3.1">
 								How Admission Works
 							</NavDropdown.Item>
@@ -38,10 +55,16 @@ export const MenuBar = () => {
 							</NavDropdown.Item>
 						</NavDropdown>
 
-						<Nav.Link href="#link">Testimonials</Nav.Link>
+						<Nav.Link className="b-right b-left px-4" href="#link">
+							Testimonials
+						</Nav.Link>
 
 						{/* Fundings dropdown */}
-						<NavDropdown title="Funding" id="basic-nav-dropdown">
+						<NavDropdown
+							className="b-right b-left px-4"
+							title="Funding"
+							id="basic-nav-dropdown"
+						>
 							<NavDropdown.Item href="#action/3.1">
 								Funding Partners
 							</NavDropdown.Item>
@@ -50,7 +73,9 @@ export const MenuBar = () => {
 							</NavDropdown.Item>
 						</NavDropdown>
 
-						<Nav.Link href="#link">Donate Now</Nav.Link>
+						<Nav.Link className="b-left b-end px-4" href="#link">
+							Donate Now
+						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
