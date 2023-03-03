29fea513c77dd8b4ce569cfba133d18b491e35d4
Tersia Koetzee <tersiakoetzee@gmail.com>
2023-03-03 21:08:58 +0200

    fixed text, columns and links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -13,7 +13,7 @@ export const Footer = () => {
 			<div className="main-footer">
 				<Container ClassName="text-center py-1">
 					<Row>
-						<Col className="icon col-md-3 col-sm-3 ">
+						<Col className="icon " sm={12} md={3}>
 							<img
 								className=" icon rounded-circle text-center py-2"
 								src="https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/328616358_556590029746692_1897079217511533681_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=W5TFkG91gMMAX-YUo9v&_nc_ht=scontent-cpt1-1.xx&oh=00_AfCk3QeJAeZ9IU3FnOXnJ8MehG4Aj10GL6qtJrATFF6ptQ&oe=640714B3"
@@ -25,17 +25,22 @@ export const Footer = () => {
 								est. 2007
 							</p>
 						</Col>
-						<Col className="col-md-3 col-sm-3">
+						<Col sm={12} md={3}>
 							<h4>More Projects</h4>
-							<a href="/projects">New Building</a>
+							<a style={{ color: "white" }} href="/projects">
+								New Building
+							</a>
 						</Col>
-						<Col className="col-md-3 col-sm-3">
+						<Col sm={12} md={3}>
 							<h4>Socials</h4>
-							<a href="https://www.facebook.com/HOPEAGAINRECOVERYHOME/">
+							<a
+								style={{ color: "white" }}
+								href="https://www.facebook.com/HOPEAGAINRECOVERYHOME/"
+							>
 								Visit FaceBook
 							</a>
 						</Col>
-						<Col className="col-md-3 col-sm-3">
+						<Col sm={12} md={3}>
 							<h4>Contact Us</h4>
 							<p>
 								021 371 7323 57 Artemis Road, Woodlands, Mitchells Plain
