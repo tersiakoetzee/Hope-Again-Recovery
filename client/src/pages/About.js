c3396976be030ce95b7871c54961a5db1d42a573
Anthony Mogotlane <almogotlane@gmail.com>
2023-03-04 05:11:07 +0200

    Imported AboutContent component

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,39 +1,10 @@
 import { Template } from "../components/Template";
+import { AboutContent } from "../components/AboutContent";
+
 export const About = () => (
 	<main role="main">
 		<Template>
-			<h1>About</h1>
-			<p>
-				Starter kit for full-stack JavaScript projects. For more information,
-				see the wiki:
-			</p>
-			<a href="https://github.com/textbook/starter-kit/wiki">Wiki</a>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Sapien nec sagittis
-				aliquam malesuada bibendum arcu vitae elementum. Mi ipsum faucibus vitae
-				aliquet nec. Eget nunc lobortis mattis aliquam faucibus purus in. Eget
-				egestas purus viverra accumsan. Vitae tortor condimentum lacinia quis
-				vel eros donec. Auctor augue mauris augue neque. Porta lorem mollis
-				aliquam ut porttitor leo a diam sollicitudin. Volutpat odio facilisis
-				mauris sit amet. In aliquam sem fringilla ut. Vel facilisis volutpat est
-				velit egestas dui. Magna fermentum iaculis eu non. Adipiscing tristique
-				risus nec feugiat.
-			</p>
-			<p>
-				Sociis natoque penatibus et magnis dis parturient montes. Porta nibh
-				venenatis cras sed. Lacinia quis vel eros donec ac odio tempor orci
-				dapibus. Donec pretium vulputate sapien nec sagittis aliquam malesuada.
-				Vulputate ut pharetra sit amet aliquam id diam maecenas ultricies.
-				Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor
-				posuere. Vulputate enim nulla aliquet porttitor lacus. At tempor commodo
-				ullamcorper a lacus. Vitae aliquet nec ullamcorper sit. Purus faucibus
-				ornare suspendisse sed nisi lacus sed. Turpis egestas maecenas pharetra
-				convallis posuere morbi leo urna. At ultrices mi tempus imperdiet nulla
-				malesuada. Praesent tristique magna sit amet purus. Aenean vel elit
-				scelerisque mauris. Pharetra magna ac placerat vestibulum lectus mauris
-				ultrices eros in.
-			</p>
+			<AboutContent />
 		</Template>
 	</main>
 );
