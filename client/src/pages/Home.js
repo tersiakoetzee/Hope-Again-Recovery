0e05749c546b99e1695f147d8d42008ced8a0575
Chezlin Benson <102852227+chezlinbenson@users.noreply.github.com>
2023-03-05 22:29:44 +0200

    Revert "#6 - Added Testimonial Widget - Chezlin Benson (#33)"
    This reverts commit f4b9b3a0e5c0c419b151c9bd7ae12a5f9664a9d2.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,9 @@
 import { MenuBar } from "../components/MenuBar";
 import "./Home.css";
 import { PicCarousel } from "../components/PicCarousel";
-import { TestimonialWidget } from "../components/TestimonialWidget";
 import { VideoWidget } from "../components/VideoWidget";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 export function Home() {
 	return (
@@ -19,19 +15,7 @@ export function Home() {
 			<main role="main">
 				<div>
 					<PicCarousel />
-					<section className="video-testimonial">
-						<Container>
-							<Row>
-								<Col className=".me-1">
-									<VideoWidget />
-								</Col>
-								<Col className=".ms-1">
-									<TestimonialWidget />
-								</Col>
-							</Row>
-						</Container>
-					</section>
-
+					<VideoWidget />
 					<Footer />
 				</div>
 			</main>
