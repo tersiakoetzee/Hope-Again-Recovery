976458fe9b6262d8599382d7952f97b6b254b690
chezlinbenson <chezlinbenson11@gmail.com>
2023-03-11 23:22:00 +0200

    Resolved CI Checks Errors

--- client/src/pages/Testimonials.js
+++ client/src/pages/Testimonials.js
@@ -7,53 +7,51 @@ import Test4 from "../assets/Test4.jpg";
 import "./Testimonials.css";
 
 export const Testimonials = () => {
-return (
-	<Template>
-		<section id="testimonial-cards">
-			<Card className="text-center" id="testimonial-cards-header">
-				<Card.Title id="testimonial-cards-title">
-					Testimonials
-				</Card.Title>
-				<Card.Text>
-					With supporting text below as a natural lead-in to additional content.
-				</Card.Text>
-			</Card>
-			<CardGroup>
-				<Card>
-					<Card.Img variant="top" src={Test2} />
-					<Card.Body>
-						<Card.Title>Samantha, 26yrs</Card.Title>
-						<Card.Text>
-							This is a wider card with supporting text below as a natural
-							lead-in to additional content. This content is a little bit
-							longer.
-						</Card.Text>
-					</Card.Body>
+	return (
+		<Template>
+			<section id="testimonial-cards">
+				<Card className="text-center" id="testimonial-cards-header">
+					<Card.Title id="testimonial-cards-title">Testimonials</Card.Title>
+					<Card.Text>
+						With supporting text below as a natural lead-in to additional
+						content.
+					</Card.Text>
 				</Card>
-				<Card>
-					<Card.Img variant="top" src={Test3} />
-					<Card.Body>
-						<Card.Title>Nathan, 19yrs</Card.Title>
-						<Card.Text>
-							This card has supporting text below as a natural lead-in to
-							additional content.{" "}
-						</Card.Text>
-					</Card.Body>
-				</Card>
-				<Card>
-					<Card.Img variant="top" src={Test4} />
-					<Card.Body>
-						<Card.Title>David, 33yrs</Card.Title>
-						<Card.Text>
-							This is a wider card with supporting text below as a natural
-							lead-in to additional content. This card has even longer content
-							than the first to show that equal height action.
-						</Card.Text>
-					</Card.Body>
-				</Card>
-			</CardGroup>
-		</section>
-	</Template>
-);
+				<CardGroup>
+					<Card>
+						<Card.Img variant="top" src={Test2} />
+						<Card.Body>
+							<Card.Title>Samantha, 26yrs</Card.Title>
+							<Card.Text>
+								This is a wider card with supporting text below as a natural
+								lead-in to additional content. This content is a little bit
+								longer.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+					<Card>
+						<Card.Img variant="top" src={Test3} />
+						<Card.Body>
+							<Card.Title>Nathan, 19yrs</Card.Title>
+							<Card.Text>
+								This card has supporting text below as a natural lead-in to
+								additional content.{" "}
+							</Card.Text>
+						</Card.Body>
+					</Card>
+					<Card>
+						<Card.Img variant="top" src={Test4} />
+						<Card.Body>
+							<Card.Title>David, 33yrs</Card.Title>
+							<Card.Text>
+								This is a wider card with supporting text below as a natural
+								lead-in to additional content. This card has even longer content
+								than the first to show that equal height action.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</CardGroup>
+			</section>
+		</Template>
+	);
 };
-
