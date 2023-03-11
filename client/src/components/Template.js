1a2ab9eee804d4a38b27b033c1d4f0b2ec681f79
Anthony Mogotlane <almogotlane@gmail.com>
2023-03-12 00:41:57 +0200

    Removed Header component

--- client/src/components/Template.js
+++ client/src/components/Template.js
@@ -1,11 +1,9 @@
 import { MenuBar } from "./MenuBar";
 import { Footer } from "./Footer";
-import { Header } from "./Header";
 
 export const Template = (props) => (
 	<main role="main">
 		<div>
-			<Header />
 			<MenuBar />
 			{props.children}
 			<Footer />
