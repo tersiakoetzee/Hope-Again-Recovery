3a39ac9641d10d4a3a5c28d2cdd444a492058e5f
chezlinbenson <chezlinbenson11@gmail.com>
2023-03-13 05:19:00 +0200

    created get route to fetch caleendar api events added server variables and api credentials

--- server/booking.js
+++ server/booking.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import { google } from "googleapis";
 
+//Calender Event List data ******
+const fs = require("fs").promises;
+const path = require("path");
+const process = require("process");
+const { authenticate } = require("@google-cloud/local-auth");
+//************************************
+
 import logger from "./utils/logger";
 import dayjs from "dayjs";
 
@@ -121,30 +128,96 @@ router.get("/", async (req, res) => {
 	// Set credentials - if not needed uncomment
 	oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
-	const results = await calendar.events.list({
-		calendarId: "primary",
-		auth: oauth2Client,
-		timeMin: new Date().toISOString(),
-		maxResults: 10, //extract the next ten events
-		singleEvents: true,
-		orderBy: "startTime",
-	});
+	// If modifying these scopes, delete token.json.
+	const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	const TOKEN_PATH = path.join(process.cwd(), "token.json");
+	const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 
-	const events = results.data.items;
-	if (!events || events.length === 0) {
+	/**
+	 * Reads previously authorized credentials from the save file.
+	 *
+	 * @return {Promise<OAuth2Client|null>}
+	 */
+	async function loadSavedCredentialsIfExist() {
+		try {
+			const content = await fs.readFile(TOKEN_PATH);
+			const credentials = JSON.parse(content);
+			return google.auth.fromJSON(credentials);
+		} catch (err) {
+			return null;
+		}
+	}
+
+	/**
+	 * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
+	 *
+	 * @param {OAuth2Client} client
+	 * @return {Promise<void>}
+	 */
+	async function saveCredentials(client) {
+		const content = await fs.readFile(CREDENTIALS_PATH);
+		const keys = JSON.parse(content);
+		const key = keys.installed || keys.web;
+		const payload = JSON.stringify({
+			type: "authorized_user",
+			client_id: key.client_id,
+			client_secret: key.client_secret,
+			refresh_token: client.credentials.refresh_token,
+		});
+		await fs.writeFile(TOKEN_PATH, payload);
+	}
+
+	/**
+	 * Load or request or authorization to call APIs.
+	 *
+	 */
+	async function authorize() {
+		let client = await loadSavedCredentialsIfExist();
+		if (client) {
+			return client;
+		}
+		client = await authenticate({
+			scopes: SCOPES,
+			keyfilePath: CREDENTIALS_PATH,
+		});
+		if (client.credentials) {
+			await saveCredentials(client);
+		}
+		return client;
+	}
+
+	/**
+	 * Lists the next 10 events on the user's primary calendar.
+	 * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+	 */
+	async function listEvents(auth) {
+		const calendar = google.calendar({ version: "v3", auth });
+		const results = await calendar.events.list({
+			calendarId: "primary",
+			timeMin: new Date().toISOString(),
+			maxResults: 10,
+			singleEvents: true,
+			orderBy: "startTime",
+		});
+		const events = results.data.items;
+		if (!events || events.length === 0) {
+			// eslint-disable-next-line no-console
+			console.log("No upcoming events found.");
+			return;
+		}
 		// eslint-disable-next-line no-console
-		console.log("No upcoming events found.");
-		return;
+		console.log("Upcoming 10 events:");
+		events.map((event) => {
+			const start = event.start.dateTime || event.start.date;
+			// eslint-disable-next-line no-console
+			console.log(`${start} - ${event.summary}`);
+		});
 	}
 	// eslint-disable-next-line no-console
-	console.log("Upcoming 10 events:");
-	events.map((event) => {
-		const start = event.start.dateTime || event.start.date;
-		// eslint-disable-next-line no-console
-		console.log(`${start} - ${event.summary}`);
-	});
-
-	//authorize().then(listEvents).catch(console.error);
+	authorize().then(listEvents).catch(console.error);
 	//GET https://www.googleapis.com/calendar/v3/calendars/primary/events/eventId
 });
 
