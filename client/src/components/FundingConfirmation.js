8a54b84aadaa39506727a8982f5789cccd18ca0a
Nishka Kisten <nishka.crypto@gmail.com>
2023-03-14 17:15:07 +0200

    Update FundingConfirmation.js

--- client/src/components/FundingConfirmation.js
+++ client/src/components/FundingConfirmation.js
@@ -6,80 +6,45 @@ export const FundingConfirmation = ({
 	country,
 	contactNumber,
 	name,
-	confirmation,
 	closeConfirmation,
 }) => {
 	return (
-		<>
-			{confirmation ? (
-				<div>
-					<Modal show={true} backdrop="static" keyboard={false}>
-						<Modal.Body>
-							<div className="text-center">
-								<Modal.Title>Submission Successful!</Modal.Title>
-								<i className="bi bi-check-circle-fill b-icon text-success"></i>
-							</div>
-
-							<h5>Your Details</h5>
-							<Card className="mb-4 p-2">
-								<p className="mb-0">Full name: {name}</p>
-								<p className="mb-0">Company Name: {companyName}</p>
-								<p className="mb-0">Contact Number: {contactNumber}</p>
-								<p className="mb-0">Country: {country}</p>
-							</Card>
-
-							<h5>Contact Details</h5>
-							<Card className="mb-4 p-2">
-								<p className="mb-0">Tel: 021 371 7323</p>
-								<p className="mb-0">
-									Email:
-									<span className="text-primary"> harh@mounthope.co.za</span>
-								</p>
-								<p className="mb-0">
-									Location: 57 Artemis Road, Woodlands, Mitchell’s Plain
-								</p>
-							</Card>
-
-							<div className="d-flex justify-content-center flex-gap">
-								<Button
-									variant="success"
-									className="m-1"
-									onClick={closeConfirmation}
-								>
-									OK
-								</Button>
-								<a className="btn btn-dark m-1" href="/">
-									Home
-								</a>
-							</div>
-						</Modal.Body>
-					</Modal>
+		<Modal show={true} backdrop="static" keyboard={false}>
+			<Modal.Body>
+				<div className="text-center">
+					<Modal.Title>Submission Successful!</Modal.Title>
+					<i className="bi bi-check-circle-fill b-icon text-success"></i>
 				</div>
-			) : (
-				<div>
-					<Modal show={true} backdrop="static" keyboard={false}>
-						<Modal.Body>
-							<div className="text-center mb-4">
-								<Modal.Title>Submission Unsuccessful</Modal.Title>
-								<i className="bi bi-x-circle-fill b-icon text-danger"></i>
-							</div>
 
-							<p className="text-center mb-4">
-								Please reload the page and try again!
-							</p>
+				<h5>Your Details</h5>
+				<Card className="mb-4 p-2">
+					<p className="mb-0">Full name: {name}</p>
+					<p className="mb-0">Company Name: {companyName}</p>
+					<p className="mb-0">Contact Number: {contactNumber}</p>
+					<p className="mb-0">Country: {country}</p>
+				</Card>
 
-							<div className="d-flex justify-content-center flex-gap">
-								<a className="btn btn-success m-1" href="/booking">
-									Reload
-								</a>
-								<a className="btn btn-dark m-1" href="/">
-									Home
-								</a>
-							</div>
-						</Modal.Body>
-					</Modal>
+				<h5>Contact Details</h5>
+				<Card className="mb-4 p-2">
+					<p className="mb-0">Tel: 021 371 7323</p>
+					<p className="mb-0">
+						Email:
+						<span className="text-primary"> harh@mounthope.co.za</span>
+					</p>
+					<p className="mb-0">
+						Location: 57 Artemis Road, Woodlands, Mitchell’s Plain
+					</p>
+				</Card>
+
+				<div className="d-flex justify-content-center flex-gap">
+					<Button variant="success" className="m-1" onClick={closeConfirmation}>
+						OK
+					</Button>
+					<a className="btn btn-dark m-1" href="/">
+						Home
+					</a>
 				</div>
-			)}
-		</>
+			</Modal.Body>
+		</Modal>
 	);
 };
