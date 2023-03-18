a53a1a5c7eb93ea6fb0775359be10c3afb9ea3a4
chezlinbenson <chezlinbenson11@gmail.com>
2023-03-18 21:30:45 +0200

    Added styling to Testimonial Widget

--- client/src/components/TestimonialWidget.js
+++ client/src/components/TestimonialWidget.js
@@ -1,20 +1,18 @@
 import Card from "react-bootstrap/Card";
 import "./TestimonialWidget.css";
-import Image1 from "../assets/testimonial-widget.jpg";
+import Image1 from "../assets/testimonial7.jpg";
 
 export const TestimonialWidget = () => {
 	return (
-		<Card className="text-white" id="testimonial-widget">
+		<Card  className="text-white" id="testimonial-widget">
 			<Card.Img src={Image1} alt="Card image" />
 			<Card.ImgOverlay className="d-flex flex-column align-items-end justify-content-end">
 				<Card.Title className="card-title mx-auto" id="testimonial-title">
-					Testimonials
+					Life Changing Stories
 				</Card.Title>
 				<Card.Text className="card-para" id="testimonial-para">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-					varius enim in eros elementum tristique. Duis cursus, mi quis viverra
-					ornare, eros dolor interdum nulla, ut. Read more{" "}
-					<a href="/testimonials"> here.</a>
+					Share in Hope Agains life changing testimonials.
+					<a href="/testimonials"> Read more here.</a>
 				</Card.Text>
 			</Card.ImgOverlay>
 		</Card>
