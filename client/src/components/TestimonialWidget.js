44ae149beaf714edce2ecf3f47230526154e9f4a
chezlinbenson <chezlinbenson11@gmail.com>
2023-03-18 22:38:50 +0200

    Added styling to testimonial

--- client/src/components/TestimonialWidget.js
+++ client/src/components/TestimonialWidget.js
@@ -1,10 +1,10 @@
 import Card from "react-bootstrap/Card";
 import "./TestimonialWidget.css";
-import Image1 from "../assets/testimonial7.jpg";
+import Image1 from "../assets/testimonial8.jpg";
 
 export const TestimonialWidget = () => {
 	return (
-		<Card  className="text-white" id="testimonial-widget">
+		<Card className="text-white" id="testimonial-widget">
 			<Card.Img src={Image1} alt="Card image" />
 			<Card.ImgOverlay className="d-flex flex-column align-items-end justify-content-end">
 				<Card.Title className="card-title mx-auto" id="testimonial-title">
