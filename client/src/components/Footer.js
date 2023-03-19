9125239297c0eedadeefb504422c2393c37a8f7f
Anthony Mogotlane <almogotlane@gmail.com>
2023-03-19 20:44:55 +0200

    Updated footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,10 +5,10 @@ import "./Footer.css";
 
 export const Footer = () => {
 	return (
-		<div className="main-footer ">
-			<Container className="text-center py-1">
+		<div className="bg-dark">
+			<Container className="text-center text-white py-4">
 				<Row>
-					<Col className="icon " sm={12} md={3}>
+					<Col className="" sm={12} md={3}>
 						<img
 							className=" icon rounded-circle text-center py-2"
 							src="https://scontent-cpt1-1.xx.fbcdn.net/v/t39.30808-6/328616358_556590029746692_1897079217511533681_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=W5TFkG91gMMAX-YUo9v&_nc_ht=scontent-cpt1-1.xx&oh=00_AfCk3QeJAeZ9IU3FnOXnJ8MehG4Aj10GL6qtJrATFF6ptQ&oe=640714B3"
@@ -16,42 +16,77 @@ export const Footer = () => {
 							height="100"
 						/>
 						<p>
-							Hope Again <br></br>Recovery Home<br></br>
+							Hope Again <br />
+							Recovery Home <br />
 							est. 2007
 						</p>
 					</Col>
 					<Col sm={12} md={3}>
-						<h4>More Projects</h4>
-						<a style={{ color: "white" }} href="/projects">
-							New Building
-						</a>
+						<h4>Useful Links</h4>
+						<hr />
+						<p>
+							<a href="/projects">
+								New Building
+							</a>
+						</p>
+						<p>
+							<a href="/booking">
+								Book Screening
+							</a>
+						</p>
+						<p>
+							<a href="/">
+								Donation
+							</a>
+						</p>
 					</Col>
 					<Col sm={12} md={3}>
 						<h4>Socials</h4>
-						<a
-							style={{ color: "white" }}
-							href="https://www.facebook.com/HOPEAGAINRECOVERYHOME/"
-						>
-							Visit FaceBook
-						</a>
+						<hr />
+						<p>
+							<a
+								href="https://www.facebook.com/HOPEAGAINRECOVERYHOME/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Facebook
+							</a>
+						</p>
 					</Col>
 					<Col sm={12} md={3}>
-						<h4>Contact Us</h4>
+						<h4>Contact Info</h4>
+						<hr />
 						<p>
-							021 371 7323 57<br></br> Artemis Road, Woodlands, Mitchells Plain
-							secretary@mounthope.co.za
+							<i className="bi bi-telephone-fill"></i> 021 371 7323
+						</p>
+						<p>
+							<i className="bi bi-envelope-fill"></i> secretary@mounthope.co.za
+						</p>
+						<p>
+							<i className="bi bi-geo-alt-fill"></i> 57 Artemis Road Woodlands
+							Mitchells Plain
 						</p>
 					</Col>
 				</Row>
-				<Row className="footer-bottom">
+				<Row>
 					<Col>
-						<p>Copyright &copy; {new Date().getFullYear()} H-A-R-H</p>
-					</Col>
-					<Col>
-						<p className=" text-center">Privacy Policy</p>
-					</Col>
-					<Col>
-						<p className=" text-center">Terms & Conditions</p>
+						<hr />
+						<a
+							className=""
+							href="https://www.facebook.com/HOPEAGAINRECOVERYHOME/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<i className="bi bi-facebook icon"></i>
+						</a>
+						<p>
+							<a href="/#">Copyright &copy; {new Date().getFullYear()} H-A-R-H</a>
+							<span className="text-warning"> | </span>
+							<a href="#/">Privacy Policy</a>
+							<span className="text-warning"> | </span>
+							<a href="/#">Terms & Conditions</a>
+
+						</p>
 					</Col>
 				</Row>
 			</Container>
